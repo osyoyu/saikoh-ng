1c7bfe8b36d1249e35ad938b772413b8894e54f4
osyoyu <osyoyu@osyoyu.com>
2015-10-14 18:35:14 +0900

    Add routing to events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'events#show', defaults: {id: 1}
+
+  resources :events, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
