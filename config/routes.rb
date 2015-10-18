bdcdbdb7f6d844c2df5992d62058b25813fff764
osyoyu <osyoyu@osyoyu.com>
2015-10-19 08:25:52 +0900

    Route events/new, edit, index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'events#show', defaults: {id: 1}
 
-  resources :events, only: [:show]
+  resources :events, only: [:index, :show, :edit, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
