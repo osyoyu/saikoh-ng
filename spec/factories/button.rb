ca31d72645bba4eff6f0ce9ca3dabcc2abd7fa8e
osyoyu <osyoyu@osyoyu.com>
2015-10-19 15:49:28 +0900

    Add Factories for Event ahd Button

---
+++ spec/factories/button.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :button do
+    name "最高"
+  end
+end
---
+++ spec/factories/events.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :event do
+    transient do
+      buttons_count 3
+    end
+
+    name "saikoh.tk"
+
+
+    after(:build) do |event, evaluator|
+      event.buttons << FactoryGirl.build_list(:button, evaluator.buttons_count, name: "最高")
+    end
+
+  end
+end
